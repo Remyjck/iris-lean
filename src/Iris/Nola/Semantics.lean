db629bcddc3a40d884c5fb171f3a3db56118830a
Remyjck <remy.seassau@gmail.com>
2025-07-14 14:41:29 +0100

    Split Nola.lean into syntax and semantics

--- src/Iris/Nola/Semantics.lean
+++ src/Iris/Nola/Semantics.lean
@@ -6,28 +6,7 @@ import Iris.Algebra.IProp
 import Iris.Instances.UPred.Instance
 import Iris.Algebra.Own
 
-inductive cif_binsel where
-| /- Conjunction -/ cifs_and
-| /- Disjunction -/ cifs_or
-| /- Implication -/ cifs_imp
-| /- Separating conjunction -/ cifs_sep
-| /- Magic wand -/ cifs_wand
-
-inductive cif_unsel where
-| /- Plainly -/ cifs_plain
-| /- Persistently -/ cifs_pers
-| /- Basic update -/ cifs_bupd
-| /- Except-0 -/ cifs_except0
-
-inductive cif (FF : Iris.GFunctors) : Type (u + 1) where
-| /- Universal quantifier -/ cifs_all (A : Type u) (Φ : A -> cif FF)
-| /- Existential quantifier -/ cifs_ex (A : Type u) (Φ : A -> cif FF)
-| /- Binary operator -/ cifs_bin (s : cif_binsel) (P Q : cif FF)
-| /- Unary operator -/ cifs_un (s : cif_unsel) (P : cif FF)
-| /- Pure proposition -/ cifs_pure (P : Prop)
-| /- Later -/ cifs_later (iP : Iris.IProp FF)
-| /- Invariant -/ cifs_inv (fml : cif FF)
-| /- Custom selector -/ cifs_own [Iris.CMRA A] [inG FF A] (a : A)
+import Iris.Nola.Syntax
 
 section noliris
 variable (FF : Iris.GFunctors)
