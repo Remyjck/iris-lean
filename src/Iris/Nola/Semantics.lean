f2f21f095366e6c754af35812de3f33fefae28b8
Remyjck <remy.seassau@gmail.com>
2025-07-21 17:07:24 +0100

    Semantic alteration? Need to sleep on this

--- src/Iris/Nola/Semantics.lean
+++ src/Iris/Nola/Semantics.lean
@@ -5,6 +5,7 @@ import Iris.Algebra.UPred
 import Iris.Algebra.IProp
 import Iris.Instances.UPred.Instance
 import Iris.Algebra.Own
+import Iris.ProofMode
 
 import Iris.Nola.Syntax
 import Iris.Nola.Inv
@@ -37,5 +38,72 @@ instance cif_inhabited : Inhabited (cif FF) where
   default := cif.cifs_pure True
 
 notation "⟦" f "⟧" => cif_sem f
+
+-- axiom sinv_unfold {FF} p (F : cif FF) :
+--   ∃ (A : Type) (_ : Iris.CMRA A) (_ : inG FF A) (a : cif FF -> A),
+--   sinv_tok p F = own (a F)
+
+-- theorem cif_sem_lift (F : cif FF) :
+--   iprop(⟦ lift_cif_to F ⟧ ⊣⊢ ⟦ F ⟧) := by
+--   induction F with
+--   | cifs_all A Φ IH =>
+--     simp [cif_sem, lift_cif_to]
+--     constructor <;> iintro h <;> iintro a
+--     · ispecialize h (ULift.up a); simp []; istop
+--       obtain ⟨ IH, _ ⟩ := IH a
+--       apply Iris.BI.entails_trans.trans Iris.BI.emp_sep.1 IH
+--     · ispecialize h (a.down); istop
+--       obtain ⟨ _, IH ⟩ := IH a.down
+--       apply Iris.BI.entails_trans.trans Iris.BI.emp_sep.1 IH
+--   | cifs_ex A Φ IH =>
+--     simp [cif_sem, lift_cif_to]
+--     constructor <;> iintro ⟨ a, h ⟩
+--     · iexists (a.down); istop; apply (IH _).1
+--     · iexists (ULift.up a); simp []; istop; apply (IH _).2
+--   | cifs_bin s P Q IHP IHQ =>
+--     cases s <;> simp [cif_sem, lift_cif_to]
+--     · apply (Iris.BI.and_congr IHP IHQ)
+--     · apply (Iris.BI.or_congr IHP IHQ)
+--     · apply (Iris.BI.imp_congr IHP IHQ)
+--     · apply (Iris.BI.sep_congr IHP IHQ)
+--     · apply (Iris.BI.wand_congr IHP IHQ)
+--   | cifs_un s P IHP =>
+--     cases s <;> simp [cif_sem, lift_cif_to]
+--     · constructor <;> apply Iris.BIPlainly.mono; apply IHP.1; apply IHP.2
+--     · apply (Iris.BI.intuitionistically_congr IHP)
+--     · constructor <;> apply Iris.BIUpdate.mono; apply IHP.1; apply IHP.2
+--     · unfold Iris.BI.BIBase.except0
+--       apply Iris.BI.or_congr; apply BIBase.BiEntails.rfl; apply IHP
+--   | cifs_pure φ =>
+--     simp [cif_sem, lift_cif_to]
+--   | cifs_later iP =>
+--     simp [cif_sem, lift_cif_to]
+--   | cifs_inv N F IH =>
+--     simp [cif_sem, lift_cif_to]
+--     unfold inv_tok
+--     apply Iris.BI.exists_congr; intros p
+--     apply Iris.BI.sep_congr; apply BIBase.BiEntails.rfl
+--     have ⟨ _, _, _, _, HF ⟩ := sinv_unfold p F
+--     have ⟨ _, _, _, _, HFlift ⟩ := sinv_unfold p (lift_cif_to F)
+--     rewrite [HF, HFlift]
+--     sorry
+--   | cifs_own a =>
+--     simp [lift_cif_to]; apply BIBase.BiEntails.rfl
+
+@[simp]
+theorem cif_wandiff_sem (P Q : @cif.{u1} FF) :
+  ⟦ cifs_wandiff P Q ⟧ = iprop(⟦ P ⟧ ∗-∗ ⟦ Q ⟧) := by
+  simp [cifs_wandiff, cif_sem]
+
+theorem semantic_alteration (P Q : cif FF) :
+  ⊢
+  □ (⟦ lift_cif_to P ⟧ ∗-∗ ⟦ lift_cif_to Q ⟧) -∗
+  (⟦ cif_inv.{u} N P ⟧ -∗ ⟦ cif_inv.{u} N Q ⟧) := by
+  simp [cif_sem, cif_inv]
+  iintro #Hequiv ⟨ P', Hequivinv, Hinv ⟩
+  iexists P'
+  isplit l [Hequivinv]
+  · sorry
+  · iexact Hinv
 
 end noliris
--- src/Iris/Nola/Syntax.lean
+++ src/Iris/Nola/Syntax.lean
@@ -15,12 +15,48 @@ inductive cif_unsel where
 | /- Basic update -/ cifs_bupd
 | /- Except-0 -/ cifs_except0
 
+set_option pp.universes true
+
 inductive cif (FF : Iris.GFunctors) : Type (u + 1) where
 | /- Universal quantifier -/ cifs_all (A : Type u) (Φ : A -> cif FF)
 | /- Existential quantifier -/ cifs_ex (A : Type u) (Φ : A -> cif FF)
-| /- Binary operator -/ cifs_bin (s : cif_binsel) (P Q : cif FF)
+| /- Binary operator -/ cifs_bin (s : cif_binsel) (P : cif FF) (Q : cif FF)
 | /- Unary operator -/ cifs_un (s : cif_unsel) (P : cif FF)
 | /- Pure proposition -/ cifs_pure (P : Prop)
 | /- Later -/ cifs_later (iP : Iris.IProp FF)
 | /- Invariant -/ cifs_inv (N : Namespace) (fml : cif FF)
 | /- Custom selector -/ cifs_own [Iris.CMRA A] [inG FF A] (a : A)
+
+
+#print cif
+
+open ULift
+
+mutual
+  def lift_cif_to {FF : Iris.GFunctors} : cif.{u} FF → cif.{max u w} FF
+    | cif.cifs_all (A : Type u) Φ =>
+        cif.cifs_all (ULift.{w, u} A) (λ a => lift_cif_to (Φ a.down))
+    | cif.cifs_ex A Φ =>
+        cif.cifs_ex (ULift.{w, u} A) (λ a => lift_cif_to (Φ a.down))
+    | cif.cifs_bin s P Q => cif.cifs_bin s (lift_cif_to P) (lift_cif_to Q)
+    | cif.cifs_un s P    => cif.cifs_un s (lift_cif_to P)
+    | cif.cifs_pure P    => cif.cifs_pure P
+    | cif.cifs_later iP  => cif.cifs_later iP
+    | cif.cifs_inv N P   => cif.cifs_inv N (lift_cif_to P)
+    | @cif.cifs_own _ A _ inst a => @cif.cifs_own _ A _ inst a
+end
+
+def cifs_wandiff {FF} (P Q : cif FF) : cif FF :=
+  cif.cifs_bin cif_binsel.cifs_and
+    (cif.cifs_bin cif_binsel.cifs_wand P Q)
+    (cif.cifs_bin cif_binsel.cifs_wand Q P)
+
+
+def cif_inv {FF} (N : Namespace) (F : cif.{u} FF) : cif.{u + 1} FF :=
+  -- ∃ Q,
+  cif.cifs_ex.{u + 1} (cif.{u} FF) fun Q =>
+    cif.cifs_bin.{u + 1} cif_binsel.cifs_sep
+      -- Q ∗-∗ F
+      ((cifs_wandiff (lift_cif_to.{u, u + 1} Q) (lift_cif_to.{u, u + 1}  F)))
+      -- inv N Q
+      (cif.cifs_inv.{u + 1} N (lift_cif_to.{u, u + 1} Q))
