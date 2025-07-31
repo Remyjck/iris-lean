83cb28cd33e89a49c3474ab79bec41e9c2007ed7
Remyjck <remy.seassau@gmail.com>
2025-07-31 17:55:14 +0100

    Define sForall and sExists (requires univ poly)

--- src/Iris/Nola/Syntax.lean
+++ src/Iris/Nola/Syntax.lean
@@ -15,15 +15,37 @@ inductive cif.unsel where
 | /- Basic update -/ bupd
 | /- Except-0 -/ except0
 
+set_option pp.universes true
+
 inductive cif.{u} (FF : Iris.GFunctors) : Type (u + 1) where
-| /- Universal quantifier -/ all {A : Type u} (Φ : A -> cif FF)
-| /- Existential quantifier -/ ex {A : Type u} (Φ : A -> cif FF)
-| /- Binary operator -/ bin (s : cif.binsel) (P : cif FF) (Q : cif FF)
-| /- Unary operator -/ un (s : cif.unsel) (P : cif FF)
-| /- Pure proposition -/ pure (P : Prop)
-| /- Later -/ later (iP : Iris.IProp FF)
-| /- Invariant -/ inv (N : Namespace) (fml : cif FF)
-| /- Custom selector -/ own [Iris.CMRA A] [inG FF A] (a : A)
+| all {A : Type u} (Φ : A → cif FF)
+| ex  {A : Type u} (Φ : A → cif FF)
+| bin (s : cif.binsel) (P : cif FF) (Q : cif FF)
+| un  (s : cif.unsel) (P : cif FF)
+| pure (P : Prop)
+| later (iP : Iris.IProp FF)
+| inv (N : Namespace) (fml : cif FF)
+| own {A : Type} [Iris.CMRA A] [inG FF A] (a : A)
+
+def liftCif.{u, v} (P : cif.{u} FF) : cif.{max u v} FF :=
+  match P with
+  | @cif.all _ A Φ =>
+      -- We need to lift the domain type A from Type u to Type (max u v)
+      cif.all (fun (a : ULift.{max u v, u} A) => liftCif (Φ a.down))
+  | @cif.ex _ A Φ =>
+      cif.ex (fun (a : ULift.{max u v, u} A) => liftCif (Φ a.down))
+  | cif.bin s P Q =>
+      cif.bin s (liftCif P) (liftCif Q)
+  | cif.un s P =>
+      cif.un s (liftCif P)
+  | cif.pure P =>
+      cif.pure P
+  | cif.later iP =>
+      cif.later iP
+  | cif.inv N fml =>
+      cif.inv N (liftCif fml)
+  | cif.own a =>
+      cif.own a
 
 namespace cif
 
@@ -37,10 +59,24 @@ def pers (P : cif FF) : cif FF := cif.un unsel.pers P
 def bupd (P : cif FF) : cif FF := cif.un unsel.bupd P
 def except0 (P : cif FF) : cif FF := cif.un unsel.except0 P
 
-def and (P Q : cif FF) : cif FF := cif.bin binsel.and P Q
+def and.{u, v} (P : cif.{u} FF) (Q : cif.{v} FF) : cif.{max u v} FF :=
+  bin cif.binsel.and (liftCif P) (liftCif Q)
+
 def or (P Q : cif FF) : cif FF := cif.bin binsel.or P Q
-def imp (P Q : cif FF) : cif FF := cif.bin binsel.imp P Q
+
+def imp.{u, v} (P : cif.{u} FF) (Q : cif.{v} FF) : cif.{max u v} FF :=
+  bin cif.binsel.imp (liftCif P) (liftCif Q)
+
 def sep (P Q : cif FF) : cif FF := cif.bin binsel.sep P Q
 def wand (P Q : cif FF) : cif FF := cif.bin binsel.wand P Q
+
+def sForall.{u} (Φ : cif.{u} FF → Prop) : cif.{u + 1} FF :=
+  all (fun (p : cif FF) => imp.{u + 1, u} (pure (Φ p)) p)
+def sExists (Φ : cif.{u} FF → Prop) : cif.{u + 1} FF :=
+  ex (fun p => and.{u + 1, u} (pure (Φ p)) p)
+
+def all' {α} (P : α → cif FF) : cif FF := sForall (fun p => ∃ a, P a = p)
+def ex' {α} (P : α → cif FF) : cif FF := sExists (fun p => ∃ a, P a = p)
+
 
 end cif
